802c9404195b42d710d50e163b7a4572b4695c1b
Jace <bellcastle88@gmail.com>
2021-06-28 20:39:25 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+
+// index.js파일에 모두 담에 한번에 import 
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+  AuthWrapper,
+} from './pages'
+
+function App() {
+  return (
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+          <Route exact path='/cart'>
+            <Cart />
+          </Route>
+          <Route exact path='/products'>
+            <Products />
+          </Route>
+          <Route exact path='/products/:id' children={<SingleProduct />} />
+          <PrivateRoute exact path='/checkout'>
+            <Checkout />
+          </PrivateRoute>
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
+  )
+}
+
+export default App
